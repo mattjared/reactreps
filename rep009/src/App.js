b40e349d2506319ec7d5f0b55dd76a7a6e5e6dad
Matt Jared <mattjared@Matts-MacBook-Pro-2.local>
2016-08-18 19:23:04 -0500

    GOT IT WORKING

--- rep009/src/App.js
+++ rep009/src/App.js
@@ -6,34 +6,56 @@ var Button = props => <span onClick={props.onClick} className={props.btnColor}>{
 var ButtonGrow = React.createClass({
     getInitialState: function(){
         return {
-            blueButton: null,
-            currentClicks: 0,
+            currentBlueClicks: 0,
+            currentRedClicks: 0
         }
     },
     handleClick: function(targetState) {
         return () => {
-            return this.setState({ currentClicks: this.state.currentClicks + 1})
+            if (targetState === 'blue') {
+                console.log('blue')
+                return this.setState({ currentBlueClicks: this.state.currentBlueClicks + 1 })
+            } else if (targetState === 'red') {
+                console.log('red')
+                return this.setState({ currentRedClicks: this.state.currentRedClicks + 1 })
+            }
         }
     },
-    render: function(){
-        var currentDivs = [];
-        for (var i = 0; i < this.state.currentClicks; i++) {
-            currentDivs.push(<div className="aSingleDiv"></div>);
+    renderDivs: function() {
+        var currentBlueDivs = [];
+        var currentRedDivs = [];
+        for (var i = 0; i < this.state.currentBlueClicks; i++) {
+            currentBlueDivs.push(<div className="aSingleBlueDiv"></div>);
         };
+        for (var j = 0; j < this.state.currentRedClicks; j++) {
+            currentRedDivs.push(<div className="aSingleRedDiv"></div>);
+        };
+        return (
+            <div>
+                <p>Blue Clicks: {this.state.currentBlueClicks}</p>
+                <p>Red Clicks: {this.state.currentRedClicks}</p>
+                <div>
+                    { currentBlueDivs }
+                    { currentRedDivs }
+                </div>
+            </div>
+        )
+
+    },
+    render: function(){
         return (
             <div>
                 <div className="content">
                     <div className="fifty">
-                        <Button text="BLUE BUTTON" btnColor="blue" onClick={this.handleClick()}/>
+                        <Button text="BLUE BUTTON" btnColor="blue" onClick={this.handleClick('blue')}/>
                     </div>
                     <div className="fifty">
-                        <Button text="RED BUTTON" btnColor="red" onClick={this.handleClick()}/>
+                        <Button text="RED BUTTON" btnColor="red" onClick={this.handleClick('red')}/>
                     </div>
                 </div>
                 <div className="content text-center">
-                    {this.state.currentClicks}
-                    <div class="div-contain">
-                        {currentDivs}
+                    <div className="div-contain">
+                        {this.renderDivs()}
                     </div>
                 </div>
             </div>
