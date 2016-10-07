5282aac6be4927c3ea6938c2031b9bed53815926
mattjared <mattjared9@gmail.com>
2016-10-06 20:57:25 -0500

    adding 15

---
+++ rep015/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>React Reps - Mouse Position</h2>
+        </div>
+        <input type="date" />
+      </div>
+    );
+  }
+}
+
+export default App;
