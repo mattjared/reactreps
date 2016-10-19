d2c16be6f9e511c66afb2e6cd4a1509665e49e8f
Matt Jared <mattjared@Matts-MacBook-Pro-2.local>
2016-10-19 17:30:07 -0500

    halfway through video 15

--- rep018/catch-of-the-day/src/components/AddFishForm.js
+++ rep018/catch-of-the-day/src/components/AddFishForm.js
@@ -11,11 +11,12 @@ class AddFishForm extends React.Component {
             desc: this.desc.value,
             image: this.image.value,
         }
-        console.log(fish);
+        this.props.addFish(fish);
+        this.fishForm.reset();
     }
     render() {
         return (
-            <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+            <form className="fish-edit" onSubmit={(e) => this.createFish(e)} ref={(input) => this.fishForm = input}>
                 <input ref={(input) => this.name = input} type="text" placeholder="Fish Name"/>
                 <input ref={(input) => this.price = input} type="text" placeholder="Fish Pric"/>
                 <select ref={(input) => this.status = input}>
