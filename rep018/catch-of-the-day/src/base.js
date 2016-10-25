af249cf31e0c77fe0ff9cf085110b7c77971dac8
Matt Jared <mattjared@Matts-MacBook-Pro-2.local>
2016-10-25 15:18:36 -0500

    video 19 => local storage

---
+++ rep018/catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyD0XikV1zccWJPdzGxBMos1yY4b3AWiGjQ",
+    authDomain: "catch-of-the-day-matt-jared.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-matt-jared.firebaseio.com",
+});
+
+export default base;
--- rep018/catch-of-the-day/src/components/App.js
+++ rep018/catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
     constructor() {
@@ -15,6 +16,26 @@ class App extends React.Component {
             fishes: {},
             order: {}
         };
+    }
+    componentWillMount() {
+        this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+        ,{
+            context: this,
+            state: 'fishes'
+        });
+
+        const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+        if (localStorageRef) {
+            this.setState({
+                order: JSON.parse(localStorageRef)
+            });
+        }
+    }
+    componentWillUnMount() {
+        base.removeBinding(this.ref);
+    }
+    componentWillUpdate(nextProps, nextState){
+        localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
     }
     addFish(fish) {
         const fishes = {...this.state.fishes};
@@ -45,7 +66,11 @@ class App extends React.Component {
                         }
                     </ul>
                 </div>
-                    <Order fishes={this.state.fishes} order={this.state.order}/>
+                    <Order
+                        fishes={this.state.fishes}
+                        order={this.state.order}
+                        params={this.props.params}
+                    />
                     <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         )
