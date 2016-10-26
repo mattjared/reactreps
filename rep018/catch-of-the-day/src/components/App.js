d40c3980c12b14ebed6de2fc5179d44eebbaf35a
Matt Jared <mattjared@Matts-MacBook-Pro-2.local>
2016-10-26 13:07:30 -0500

    finish video 21

--- rep018/catch-of-the-day/src/components/App.js
+++ rep018/catch-of-the-day/src/components/App.js
@@ -11,8 +11,10 @@ class App extends React.Component {
         super();
         this.addFish = this.addFish.bind(this);
         this.updateFish = this.updateFish.bind(this);
+        this.removeFish = this.removeFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
+        this.removeFromOrder = this.removeFromOrder.bind(this);
         this.state = {
             fishes: {},
             order: {}
@@ -49,15 +51,25 @@ class App extends React.Component {
         fishes[key] = updatedFish;
         this.setState({ fishes });
     }
+    removeFish(key) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = null;
+        this.setState({ fishes });
+    }
     loadSamples() {
         this.setState({
             fishes: sampleFishes,
         });
     }
-    addToOrder(key){
+    addToOrder(key) {
         const order = {...this.state.order};
         order[key] = order[key] + 1 || 1;
-        this.setState({order});
+        this.setState({ order });
+    }
+    removeFromOrder(key) {
+        const order = {...this.state.order};
+        delete order[key];
+        this.setState({ order });
     }
     render() {
         return (
@@ -76,9 +88,11 @@ class App extends React.Component {
                         fishes={this.state.fishes}
                         order={this.state.order}
                         params={this.props.params}
+                        removeFromOrder={this.removeFromOrder}
                     />
                     <Inventory
                         addFish={this.addFish}
+                        removeFish={this.removeFish}
                         loadSamples={this.loadSamples}
                         fishes={this.state.fishes}
                         updateFish={this.updateFish}
