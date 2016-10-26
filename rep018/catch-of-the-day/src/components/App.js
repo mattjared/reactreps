cdf7ad1667b9efcb05f2a2015226fbaff5e20421
Matt Jared <mattjared@Matts-MacBook-Pro-2.local>
2016-10-26 12:16:47 -0500

    Adding video 20

--- rep018/catch-of-the-day/src/components/App.js
+++ rep018/catch-of-the-day/src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     constructor() {
         super();
         this.addFish = this.addFish.bind(this);
+        this.updateFish = this.updateFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
         this.state = {
@@ -43,6 +44,11 @@ class App extends React.Component {
         fishes[`fish-${timestamp}`] = fish;
         this.setState({ fishes })
     }
+    updateFish(key, updatedFish) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = updatedFish;
+        this.setState({ fishes });
+    }
     loadSamples() {
         this.setState({
             fishes: sampleFishes,
@@ -71,7 +77,12 @@ class App extends React.Component {
                         order={this.state.order}
                         params={this.props.params}
                     />
-                    <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+                    <Inventory
+                        addFish={this.addFish}
+                        loadSamples={this.loadSamples}
+                        fishes={this.state.fishes}
+                        updateFish={this.updateFish}
+                    />
             </div>
         )
     }
